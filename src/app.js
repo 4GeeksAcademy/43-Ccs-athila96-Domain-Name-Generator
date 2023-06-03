6473749dde470d839b6925855735aa55e28cef8d
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-06-03 01:27:26 +0000

    Se creo una funcion que tiene como finalidad generar dominios aleatorios a partir de arreglos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// La funcion DomainGenerator se realizo con el fin de generar dominios aleatorios sin mentoria alguna.
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domainGenerator(extension) {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let ownName = 0; ownName < pronoun.length; ownName++) {
+    for (let qualifying = 0; qualifying < adj.length; qualifying++) {
+      for (let crucial = 0; crucial < noun.length; crucial++) {
+        console.log(
+          pronoun[ownName] + adj[qualifying] + noun[crucial] + extension
+        );
+      }
+    }
+  }
+}
+
+//domainGenerator(".net");
+
+// La funcion domainRamdom se realizo en clase con mentoria para mejorar el codigo y hacerlo mas simple.
+
+function domainRamdom() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".es", ".net"];
+
+  for (let ownName of pronoun) {
+    for (let qualifying of adj) {
+      for (let crucial of noun) {
+        for (let extension of extensions) {
+          console.log(ownName + qualifying + crucial + extension);
+        }
+      }
+    }
+  }
+}
+
+domainRamdom();
